62666c6720b93ada3d2625e31a7d67a7bf615906
Krystian Piekos <krystian.piekos@infotraining.pl>
2025-10-07 14:30:22 +0200

    Implement Stack class with push, pop, and range constructor; enhance tests for stack operations

--- _exercises/ex-class-templates/ex_class_templates.cpp
+++ _exercises/ex-class-templates/ex_class_templates.cpp
@@ -17,125 +17,200 @@
 #define __PRETTY_FUNCTION__ __FUNCSIG__
 #endif
 
-// TODO: Stack
+template <typename T, typename Container = std::deque<T>>
+class Stack
+{
+private:
+    Container container;
 
-// static_assert(std::is_same_v<Stack<int>::container_type, std::deque<int>>);
-// static_assert(std::is_same_v<Stack<int, std::vector<int>>::container_type, std::vector<int>>);
+public:
+    using container_type = Container;
+    using reference = T&;
+    using const_reference = const T&;
+    using value_type = T;
+
+    Stack() = default;
+
+    template <typename TRange>
+    Stack(const TRange& range)
+    {
+        for(const auto& item : range)
+            push(item);
+    }
+
+    bool empty() const;
+
+    size_t size() const
+    {
+        return container.size();
+    }
+
+    reference top() 
+    {
+        return container.back();
+    }
+
+    const_reference top() const
+    {
+        return container.back();
+    }
+
+    template <typename TArg>
+    void push(TArg&& value) // universal reference
+    {
+        container.push_back(std::forward<TArg>(value));
+    }
+
+    // void push(const T& value)
+    // {
+    //     container.push_back(value); // copy value into container 
+    // }
+
+    // void push(T&& value)
+    // {
+    //     container.push_back(std::move(value)); // move value into container
+    // }
+
+    void pop(T& value)
+    {
+        value = std::move_if_noexcept(container.back());
+        container.pop_back();
+    }
+};
+
+template <typename T, typename Container>
+bool Stack<T, Container>::empty() const
+{
+    return container.empty();
+}
+
+static_assert(std::is_same_v<Stack<int>::container_type, std::deque<int>>);
+static_assert(std::is_same_v<Stack<int, std::vector<int>>::container_type, std::vector<int>>);
 
 // // explicit template instantiation
 // template class Stack<int>;
 // template class Stack<std::string>;
 
-// TEST_CASE("After construction", "[stack]")
-// {
-//     Stack<int> s;
+TEST_CASE("After construction", "[stack]")
+{
+    Stack<int> s;
 
-//     SECTION("is empty")
-//     {
-//         REQUIRE(s.empty());
-//     }
+    SECTION("is empty")
+    {
+        REQUIRE(s.empty());
+    }
 
-//     SECTION("size is zero")
-//     {
-//         REQUIRE(s.size() == 0);
-//     }
-// }
+    SECTION("size is zero")
+    {
+        REQUIRE(s.size() == 0);
+    }
+}
 
-// TEST_CASE("Pushing an item", "[stack,push]")
-// {
-//     Stack<int> s;
+TEST_CASE("Pushing an item", "[stack,push]")
+{
+    Stack<int> s;
 
-//     SECTION("is no longer empty")
-//     {
-//         s.push(1);
+    SECTION("is no longer empty")
+    {
+        s.push(1);
 
-//         REQUIRE(!s.empty());
-//     }
+        REQUIRE(!s.empty());
+    }
 
-//     SECTION("size is increased")
-//     {
-//         auto size_before = s.size();
+    SECTION("size is increased")
+    {
+        auto size_before = s.size();
 
-//         s.push(1);
+        s.push(1);
 
-//         REQUIRE(s.size() - size_before == 1);
-//     }
+        REQUIRE(s.size() - size_before == 1);
+    }
 
-//     SECTION("recently pushed item is on a top")
-//     {
-//         s.push(4);
+    SECTION("recently pushed item is on a top")
+    {
+        s.push(4);
 
-//         REQUIRE(s.top() == 4);
-//     }
-// }
+        REQUIRE(s.top() == 4);
+    }
+}
 
-// template <typename TStack>
-// std::vector<typename TStack::value_type> pop_all(TStack& s)
-// {
-//     std::vector<typename TStack::value_type> values(s.size());
+template <typename TStack>
+std::vector<typename TStack::value_type> pop_all(TStack& s)
+{
+    std::vector<typename TStack::value_type> values(s.size());
 
-//     for (auto& item : values)
-//         s.pop(item);
+    for (auto& item : values)
+        s.pop(item);
 
-//     return values;
-// }
+    return values;
+}
 
-// TEST_CASE("Pop all items")
-// {
-//     Stack<int> s;
+TEST_CASE("Pop all items")
+{
+    Stack<int> s;
 
-//     s.push(1);
-//     s.push(4);
+    s.push(1);
+    s.push(4);
 
-//     auto values = pop_all<Stack<int>>(s);
-//     REQUIRE(values.size() == 2);
-// }
+    auto values = pop_all<Stack<int>>(s);
+    REQUIRE(values.size() == 2);
+}
 
-// TEST_CASE("Popping an item", "[stack,pop]")
-// {
-//     Stack<int> s;
+TEST_CASE("Popping an item", "[stack,pop]")
+{
+    Stack<int> s;
 
-//     int x = 1;
-//     s.push(x);
-//     s.push(4);
+    int x = 1;
+    s.push(x);
+    s.push(4);
 
-//     int item;
+    int item;
 
-//     SECTION(" an item from a top to an argument passed by ref")
-//     {
-//         s.pop(item);
+    SECTION(" an item from a top to an argument passed by ref")
+    {
+        s.pop(item);
 
-//         REQUIRE(item == 4);
-//     }
+        REQUIRE(item == 4);
+    }
 
-//     SECTION("size is decreased")
-//     {
-//         size_t size_before = s.size();
+    SECTION("size is decreased")
+    {
+        size_t size_before = s.size();
 
-//         s.pop(item);
+        s.pop(item);
 
-//         REQUIRE(size_before - s.size() == 1);
-//     }
+        REQUIRE(size_before - s.size() == 1);
+    }
 
-//     SECTION("LIFO order")
-//     {
-//         int a, b;
+    SECTION("LIFO order")
+    {
+        int a, b;
 
-//         s.pop(a);
-//         s.pop(b);
+        s.pop(a);
+        s.pop(b);
 
-//         REQUIRE(a == 4);
-//         REQUIRE(b == 1);
-//     }
-// }
+        REQUIRE(a == 4);
+        REQUIRE(b == 1);
+    }
+}
 
-// TEST_CASE("Popping all items")
-// {
-//     Stack<int, std::vector<int>> s;
+TEST_CASE("Popping all items")
+{
+    Stack<int, std::vector<int>> s;
 
-//     s.push(1);
-//     s.push(4);
+    s.push(1);
+    s.push(4);
 
-//     auto values = pop_all(s);
-//     REQUIRE(values.size() == 2);
-// }
+    auto values = pop_all(s);
+    REQUIRE(values.size() == 2);
+}
+
+TEST_CASE("Construct from range")
+{
+    std::vector<int> vec_range = {1, 2, 3, 42};
+
+    Stack<int> stack(vec_range);
+
+    REQUIRE(stack.size() == 4);
+    REQUIRE(stack.top() == 42);
+}
--- class-templates/class_templates.cpp
+++ class-templates/class_templates.cpp
@@ -3,9 +3,82 @@
 #include <string>
 #include <vector>
 
+template <typename T, size_t N>
+struct Array
+{
+    T items[N];
+
+    using iterator = T*;
+    using const_iterator = const T*;
+    using value_type = T;
+    using reference = T&;
+    using const_reference = const T&;
+
+    size_t size() const;
+
+    iterator begin()
+    {
+        return items;
+    }
+
+    iterator end()
+    {
+        return items + N;
+    }
+
+    const_iterator begin() const
+    {
+        return items;
+    }
+
+    const_iterator end() const
+    {
+        return items + N;
+    }
+
+    // reference operator[](size_t n)
+    // {
+    //     return items[n];
+    // }
+
+    // const_reference operator[](size_t n) const
+    // {
+    //     return this->items[n];
+    // }
+
+    template <typename TSelf>
+    auto& operator[](this TSelf&& self, size_t n);
+
+    auto operator<=>(const Array& other) const = default;
+};
+
+template <typename T, size_t N>
+size_t Array<T, N>::size() const
+{
+    return N;
+}
+
+template <typename T, size_t N>
+    template <typename TSelf>
+auto& Array<T, N>::operator[](this TSelf&& self, size_t n)
+{
+    return std::forward<TSelf>(self).items[n];
+}
+
 using namespace std::literals;
 
 TEST_CASE("class templates")
 {
-    CHECK(true);
-}
+    Array<int, 5> arr1 = {1, 2, 3, 4, 5};
+
+    for(int item : arr1)
+        std::cout << item << " ";
+    std::cout << "\n";
+
+    for(int& item : arr1)
+        item *= 2;
+
+    arr1[2] = 665;
+
+    REQUIRE(arr1 == Array<int, 5>{2, 4, 6, 8, 10});
+} 

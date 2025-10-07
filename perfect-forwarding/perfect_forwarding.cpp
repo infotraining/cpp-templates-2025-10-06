de5bfb7fa2278677fe333c849cb6ae013578bce6
Krystian Piekos <krystian.piekos@infotraining.pl>
2025-10-07 10:59:33 +0200

    Refactor perfect forwarding and enhance lambda usage in tests

--- perfect-forwarding/perfect_forwarding.cpp
+++ perfect-forwarding/perfect_forwarding.cpp
@@ -29,32 +29,122 @@ void have_fun(Gadget&& g)
     g.use();
 }
 
-void use(Gadget& g)
-{
-    have_fun(g);
-}
-
-void use(const Gadget& g)
-{
-    have_fun(g);
-}
-
-void use(Gadget&& g)
-{
-    have_fun(std::move(g));
-}
-
-// TEST_CASE("4---")
+// void use(Gadget& g)
 // {
-//     std::cout << "\n--------------------------\n\n";
+//     have_fun(g);
 // }
 
-// TEST_CASE("custom forwarding")
+// void use(const Gadget& g)
 // {
-//     Gadget g{1, "gadget"};
-//     const Gadget cg{2, "const-gadget"};
-
-//     use(g);
-//     use(cg);
-//     use(Gadget{3, "temp-gadget"});
+//     have_fun(g);
 // }
+
+// void use(Gadget&& g)
+// {
+//     have_fun(std::move(g));
+// }
+
+template <typename TGadget>
+void use(TGadget&& g)
+{
+    have_fun(std::forward<TGadget>(g));
+
+    // conceptually interpreted as
+    // if constexpr(std::is_lvalue_reference_v<TGadget>)
+    // {
+    //     have_fun(g);
+    // }
+    // else
+    // {
+    //     have_fun(std::move(g));
+    // }
+}
+
+namespace Cpp20
+{
+    void use(auto&& g)
+    {
+        have_fun(std::forward<decltype(g)>(g));
+    }
+}
+
+std::string get_full_name(const std::string& fn, const std::string& ln)
+{
+    return fn + " " + ln;
+}
+
+TEST_CASE("get_full_name")
+{
+    std::string name = "Jan";
+    std::string full_name = get_full_name(name, "Kowalski");
+}
+
+TEST_CASE("custom forwarding")
+{
+    Gadget g{1, "gadget"};
+    const Gadget cg{2, "const-gadget"};
+
+    use(g); 
+    use(cg);
+    use(Gadget{3, "temp-gadget"});
+}
+
+template <typename TTracer>
+auto fun_with_tracer(TTracer&& tracer)
+{
+    auto inner_arg = std::forward<TTracer>(tracer);
+    std::cout << "In fun_with_tracer: " << inner_arg << "\n";
+
+    return inner_arg; // NRVO or move to caller
+}
+
+template <typename TArg>
+auto use_lambda(TArg&& arg)
+{
+    auto lambda = [inner_arg = std::forward<TArg>(arg)] mutable { 
+        std::cout << "From lambda: " << inner_arg << "\n";
+
+        return std::move(inner_arg); // move to caller
+    };
+
+    std::cout << "From use_lambda: " << arg << "\n";
+
+    return lambda();
+}
+
+struct Tracer
+{
+    Tracer()
+    {
+        std::cout << "Tracer::Tracer()\n";
+    }
+
+    Tracer(const Tracer&)
+    {
+        std::cout << "Tracer::Tracer(const Tracer&)\n";
+    }
+
+    Tracer(Tracer&&)
+    {
+        std::cout << "Tracer::Tracer(Tracer&&)\n";
+    }
+
+    ~Tracer()
+    {
+    }
+
+    friend std::ostream& operator<<(std::ostream& os, const Tracer&)
+    {
+        return os << "Tracer instance";
+    }
+};
+
+TEST_CASE("lambda + perfect forward")
+{
+    Tracer t;
+    use_lambda(t);            
+    std::cout << "----\n";
+    auto t2 = use_lambda(Tracer{});     
+    std::cout << "----\n";
+    auto t3 = fun_with_tracer(Tracer{});       
+}

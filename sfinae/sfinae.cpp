b99ef3adff8c62bcf589e8bb2f39c44f539c2a6a
Krystian Piekos <krystian.piekos@infotraining.pl>
2025-10-08 09:16:33 +0200

    Before 3d day

--- sfinae/sfinae.cpp
+++ sfinae/sfinae.cpp
@@ -2,8 +2,21 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <cmath>
 
 using namespace std::literals;
+
+auto is_power_of_2(int value)
+{
+    return value > 0 && (value & (value - 1)) == 0;
+}
+
+auto is_power_of_2(double value)
+{
+    int exponent;
+    const double mantissa = std::frexp(value, &exponent);
+    return mantissa == 0.5;
+}
 
 TEST_CASE("SFINAE")
 {
